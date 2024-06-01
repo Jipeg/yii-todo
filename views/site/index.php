7ec5116e0e4ec6292ae062a9b4d8780f147a46cd
Nikolai Andronov <nickandronick@gmail.com>
2024-06-02 00:20:17 +0300

    todo in index view

--- views/site/index.php
+++ views/site/index.php
@@ -2,52 +2,40 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = 'My ToDos';
 ?>
-<div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
+<div class="bg-white p-5 rounded-lg shadow w-100" style="max-width: 600px;">
+    <h1 class="text-center mb-4">To-Do List</h1>
+    <form id="todo-form" class="form-inline mb-3">
+        <input id="todo-input" type="text" class="form-control flex-grow-1 mr-2" placeholder="Add a new task" required>
+        <button type="submit" class="btn btn-primary">Add</button>
+    </form>
+    <ul id="todo-list" class="list-group">
+        <!-- To-Do items will be appended here -->
+    </ul>
 </div>
+
+<script>
+    document.getElementById('todo-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const input = document.getElementById('todo-input');
+        const newTodoText = input.value.trim();
+        if (newTodoText !== "") {
+            const li = document.createElement('li');
+            li.className = "list-group-item d-flex justify-content-between align-items-center";
+            li.innerHTML = `
+                <span>${newTodoText}</span>
+                <button class="btn btn-danger btn-sm">Delete</button>
+            `;
+            document.getElementById('todo-list').appendChild(li);
+            input.value = '';
+        }
+    });
+
+    document.getElementById('todo-list').addEventListener('click', function(event) {
+        if (event.target.tagName === 'BUTTON') {
+            const li = event.target.parentElement;
+            li.remove();
+        }
+    });
+</script>
